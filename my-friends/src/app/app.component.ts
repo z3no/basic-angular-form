16a1923e08159b16b485271f2173badd6d225409
Zeno Driesen <zeno.driesen@gmail.com>
2022-08-04 10:38:42 +0200

    title myFriendspace

--- my-friends/src/app/app.component.ts
+++ my-friends/src/app/app.component.ts
@@ -1,13 +1,13 @@
 // IN THIS FILE WE'LL WRITE OUR LOGIC
-/*
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-friends';
+  title = 'myFriendspace';
 }
-*/
+
