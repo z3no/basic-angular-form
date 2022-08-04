ec7ad27bd598536dcfa2a60b45037e4854e99759
Zeno Driesen <zeno.driesen@gmail.com>
2022-08-04 15:10:53 +0200

    imported FormsModule

--- my-friends/src/app/app.module.ts
+++ my-friends/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentFormComponent } from './component-form/component-form.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ComponentFormComponent } from './component-form/component-form.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
