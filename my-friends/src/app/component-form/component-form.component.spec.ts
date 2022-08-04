2e9351fdb3ea893f337506a81a544c6a53ef707b
Zeno Driesen <zeno.driesen@gmail.com>
2022-08-04 11:24:06 +0200

    form component is born

---
+++ my-friends/src/app/component-form/component-form.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ComponentFormComponent } from './component-form.component';
+
+describe('ComponentFormComponent', () => {
+  let component: ComponentFormComponent;
+  let fixture: ComponentFixture<ComponentFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ComponentFormComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ComponentFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ my-friends/src/app/component-form/component-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-component-form',
+  templateUrl: './component-form.component.html',
+  styleUrls: ['./component-form.component.scss']
+})
+export class ComponentFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
