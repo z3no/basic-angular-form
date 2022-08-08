214add27402403157fd244a7b5496d8fabf7a67f
Zeno Driesen <zeno.driesen@gmail.com>
2022-08-08 09:15:56 +0200

    made the app.post finished exercise 16 made the post function, pushing the data from the form to the allFriends array. Tested and worked

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-const PORT = 6969;
+const PORT = 9099;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}) );
@@ -24,8 +24,16 @@ app.get('/', function (request, response) {
     response.send('Hello from server');
 });
 
+app.get('/allFriends', function (request, response) {
+    response.send(allFriends);
+});
+
 app.post('/', function (request, response) {
     response.status(200).send({"message": "Data received"});
+});
+
+app.post('/allFriends', function (request, response) {
+    allFriends.push(request.body);
 });
 
 
